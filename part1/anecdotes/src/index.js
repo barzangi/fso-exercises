55e92a37f98a15553a3e8ffaf12adfd342864c47
Layth Barzangi <thebarzangi@gmail.com>
2019-09-05 20:05:59 +0400

    Part1 exercises done

---
+++ part1/anecdotes/src/index.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = (props) => {
+
+  const [selected, setSelected] = useState(0);
+  const [votes, updateVotes] = useState(() => {
+    // initialize votes array with zero values
+    let votesInit = new Array(anecdotes.length).fill(0);
+    return votesInit;
+  });
+
+  // return random index for anecdotes array
+  const randomIndex = () => Math.floor(Math.random() * props.anecdotes.length);
+
+  // update votes array
+  const addVote = (currentSelected) => {
+    const votesCopy = [...votes];
+    votesCopy[currentSelected] += 1;
+    updateVotes(votesCopy);
+  }
+
+  // find index of top voted anecdote
+  const findTopAnecdote = () => {
+    let max = 0
+    let maxIndex = 0
+    for (let i = 0; i < votes.length; i++) {
+      if (votes[i] > max) {
+        maxIndex = i;
+        max = votes[i];
+      }
+    }
+    return maxIndex;
+  }
+
+  return (
+    <>
+      <h1>Anecdote of the day</h1>
+      <div>{props.anecdotes[selected]}</div>
+      <div>has {votes[selected]} votes</div>
+      <button onClick={() => addVote(selected)}>vote</button>
+      <button onClick={() => setSelected(randomIndex)}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <div>{props.anecdotes[findTopAnecdote()]}</div>
+      <div>has {votes[findTopAnecdote()]} votes</div>
+    </>
+  );
+}
+
+const anecdotes = [
+  'If it hurts, do it more often.',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+];
+
+ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById('root'));
---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const Header = ({text}) => <h1>{text}</h1>
+
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
+
+const Statistic = ({ text, value }) => <tr><td>{text}</td><td>{value}</td></tr>
+
+const Statistics = (props) => {
+  if (!props.good && !props.neutral && !props.bad) return <div>no feedback given</div>;
+  const all = props.good + props.neutral + props.bad;
+  const average = ((props.good + -props.bad) / (props.good + props.neutral + props.bad)).toFixed(1);
+  const positive = (props.good * 100 / (props.good + props.neutral + props.bad)).toFixed(1) + ' %';
+  return (
+    <table>
+      <tbody>
+        <Statistic text='good' value={props.good} />
+        <Statistic text='neutral' value={props.neutral} />
+        <Statistic text='bad' value={props.bad} />
+        <Statistic text='all' value={all} />
+        <Statistic text='average' value={average} />
+        <Statistic text='positive' value={positive} />
+      </tbody>
+    </table>
+  );
+}
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <>
+      <Header text='give feedback' />
+      <Button handleClick={() => setGood(good + 1)} text='good' />
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+      <Header text='statistics' />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
