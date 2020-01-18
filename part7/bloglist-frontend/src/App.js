291b820f95c455b646f9930b32c581e2691b6607
Layth Barzangi <thebarzangi@gmail.com>
2020-01-18 16:28:26 +0400

    switch blogList from component to function

--- part7/bloglist-frontend/src/App.js
+++ part7/bloglist-frontend/src/App.js
@@ -156,6 +156,7 @@ const App = (props) => {
     </form>
   )
 
+  /*
   const BlogList = () => {
     return (
       <>
@@ -171,6 +172,17 @@ const App = (props) => {
       </>
     )
   }
+  */
+  const blogList = () =>
+    props.blogs.map(blog =>
+      <Blog
+        key={blog.id}
+        blog={blog}
+        addLike={() => like(blog)}
+        destroyBlog={() => destroyBlog(blog)}
+        loggedInUser={user}
+      />
+    )
 
   return (
     <>
@@ -187,7 +199,8 @@ const App = (props) => {
             <h1>Create new</h1>
             {blogForm()}
           </Togglable>
-          <BlogList />
+          {blogList()}
+          {/* <BlogList /> */}
         </div>
       }
     </>
