55fe97b56e14c6b41fee101f15138e0e5d1ebd76
Layth Barzangi <thebarzangi@gmail.com>
2020-01-21 23:54:55 +0400

    Part4 exercises done.

--- part4/bloglist-backend/controllers/blogs.js
+++ part4/bloglist-backend/controllers/blogs.js
@@ -32,11 +32,12 @@ blogsRouter.post('/', async (req, res, next) => {
     }
 
     if (!blog.title || !blog.author || !blog.url) {
-      res.status(400).end()
+      return res.status(400).json({ error: 'please fill all form fields' })
     } else {
       const savedBlog = await blog.save()
       user.blogs = user.blogs.concat(savedBlog._id)
       await user.save()
+      console.log('savedBlog.toJSON():', savedBlog.toJSON())
       res.json(savedBlog.toJSON())
     }
   } catch(exception) {
@@ -55,9 +56,11 @@ blogsRouter.delete('/:id', async (req, res, next) => {
     const blog = await Blog.findById(req.params.id)
 
     if (blog.user.toString() !== user._id.toString()) {
-      return res.status(401).json({ error: 'this user not authorized to delete blog' })
+      return res.status(401).json({ error: 'this user is not authorized to delete blog' })
     } else {
       await Blog.findByIdAndRemove(req.params.id)
+      user.blogs = user.blogs.filter(b => b._id.toString() !== blog._id.toString())
+      await user.save()
       res.status(204).end()
     }
   } catch(exception) {
@@ -72,7 +75,8 @@ blogsRouter.put('/:id', async (req, res, next) => {
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes
+    likes: body.likes,
+    user: body.user.id
   }
 
   try {
