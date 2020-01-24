3fdd2111a993489facc6fb2b2bf80dcaac1cce5f
Layth Barzangi <thebarzangi@gmail.com>
2020-01-24 16:19:47 +0400

    add Users view. bug: destroyBlog() notification

--- part7/bloglist-frontend/src/App.js
+++ part7/bloglist-frontend/src/App.js
@@ -1,20 +1,148 @@
 import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Blog from './components/Blog'
 import Notification from './components/Notification'
 import blogService from './services/blogs'
 import loginService from './services/login'
+import usersService from './services/users'
 import Togglable from './components/Togglable'
 import { useField } from './hooks'
 import { setNotification } from './reducers/notificationReducer'
 import { initializeBlogs, createBlog, addLike, destroyBlog } from './reducers/blogReducer'
 import { setUser } from './reducers/userReducer'
 
-const App = (props) => {
+const Header = (props) => {
+  return (
+    <>
+      <h1>Blogs</h1>
+      <p>{props.user.name} is logged in <button onClick={props.handleLogout}>logout</button></p>
+    </>
+  )
+}
+
+const Blogs = (props) => {
   const [blogs, setBlogs] = useState([])
   const { value:newTitle, bind:bindNewTitle, reset:resetNewTitle } = useField('text')
   const { value:newAuthor, bind:bindNewAuthor, reset:resetNewAuthor } = useField('text')
   const { value:newUrl, bind:bindNewUrl, reset:resetNewUrl } = useField('text')
+
+  const blogForm = () => (
+    <form onSubmit={addBlog}>
+      <div>
+        title
+        <input {...bindNewTitle} />
+      </div>
+      <div>
+        author
+        <input {...bindNewAuthor} />
+      </div>
+      <div>
+        url
+        <input {...bindNewUrl} />
+      </div>
+      <button type='submit'>create</button>
+    </form>
+  )
+
+  const blogList = () =>
+    props.blogs.map(blog =>
+      <Blog
+        key={blog.id}
+        blog={blog}
+        addLike={() => like(blog)}
+        deleteBlog={() => deleteBlog(blog)}
+        loggedInUser={props.user}
+      />
+    )
+
+  const addBlog = async (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: newTitle,
+      author: newAuthor,
+      url: newUrl
+    }
+    try {
+      const newBlog = await props.createBlog(blogObject, props.user)
+      setBlogs(blogs.concat(newBlog))
+      props.setNotification(`New blog "${newTitle}" by ${newAuthor} added`, true, 5)
+    } catch (error) {
+      props.setNotification('an error occured', false, 5)
+    }
+    resetNewTitle()
+    resetNewAuthor()
+    resetNewUrl()
+  }
+
+  const like = (blog) => {
+    props.addLike(blog)
+  }
+
+  const deleteBlog = async (blog) => {
+    const deleteConfirm = window.confirm(`Remove blog post "${blog.title}" by ${blog.author}?`)
+    if (deleteConfirm) {
+      try {
+        const idToRemove = blog.id
+        await props.destroyBlog(idToRemove)
+        console.log('blog.id:', idToRemove)
+        setBlogs(blogs.filter(b => b.id !== idToRemove))
+        props.setNotification(`Removed blog post "${blog.title}" by ${blog.author}`, true, 5)
+      } catch (error) {
+        console.log('error:', error)
+        props.setNotification('an error occured', false, 5)
+      }
+    }
+  }
+
+  return (
+    <>
+      <Togglable buttonLabel='new blog'>
+        <h1>Create new</h1>
+        {blogForm()}
+      </Togglable>
+      {blogList()}
+    </>
+  )
+
+}
+
+const Users = () => {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const users = await usersService.getAll()
+      setUsers(users)
+    }
+    getUsers()
+  }, [])
+
+  console.log('users:', users)
+
+  if (users.length === 0) return null
+
+  return (
+    <>
+      <h1>Users</h1>
+      <table>
+        <thead>
+          <tr><th /><th><strong>blogs created</strong></th></tr>
+        </thead>
+        <tbody>
+          {users.map(user =>
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.blogs.length}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+const App = (props) => {
   const { value:username, bind:bindUsername, reset:resetUsername } = useField('text')
   const { value:password, bind:bindPassword, reset:resetPassword } = useField('password')
 
@@ -31,75 +159,6 @@ const App = (props) => {
     }
     // eslint-disable-next-line
   }, [])
-
-  const addBlog = async (event) => {
-    event.preventDefault()
-    const blogObject = {
-      title: newTitle,
-      author: newAuthor,
-      url: newUrl
-    }
-    try {
-      const newBlog = await props.createBlog(blogObject, props.user)
-      setBlogs(blogs.concat(newBlog))
-      props.setNotification(`New blog "${newTitle}" by ${newAuthor} added`, true, 5)
-    } catch (error) {
-      props.setNotification(error.response.data.error, false, 5)
-    }
-
-    /*
-    blogService
-      .create(blogObject)
-      .then(returnedBlog => {
-        returnedBlog = {
-          ...returnedBlog,
-          user: {
-            username: user.username,
-            name: user.name,
-            id: returnedBlog.user
-          }
-        }
-        setBlogs(blogs.concat(returnedBlog))
-        props.setNotification(`New blog "${newTitle}" by ${newAuthor} added`, true, 5)
-      })
-      .catch(error => {
-        props.setNotification(error.response.data.error, false, 5)
-      })
-    */
-
-    resetNewTitle()
-    resetNewAuthor()
-    resetNewUrl()
-  }
-
-  const like = (blog) => {
-    props.addLike(blog)
-  }
-
-  const destroyBlog = async (blog) => {
-    const destroyConfirm = window.confirm(`Remove blog post "${blog.title}" by ${blog.author}?`)
-    if (destroyConfirm) {
-      try {
-        await props.destroyBlog(blog.id)
-        setBlogs(blogs.filter(b => b.id !== blog.id))
-        props.setNotification(`Removed blog post "${blog.title}" by ${blog.author}`, true, 5)
-      } catch (error) {
-        props.setNotification(error.response.data.error, false, 5)
-      }
-
-      /*
-      blogService
-        .destroy(blog.id)
-        .then(() => {
-          setBlogs(blogs.filter(b => b.id !== blog.id))
-          props.setNotification(`Removed blog post "${blog.title}" by ${blog.author}`, true, 5)
-        })
-        .catch(error => {
-          props.setNotification(error.response.data.error, false, 5)
-        })
-      */
-    }
-  }
 
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -138,52 +197,6 @@ const App = (props) => {
     </form>
   )
 
-  const blogForm = () => (
-    <form onSubmit={addBlog}>
-      <div>
-        title
-        <input {...bindNewTitle} />
-      </div>
-      <div>
-        author
-        <input {...bindNewAuthor} />
-      </div>
-      <div>
-        url
-        <input {...bindNewUrl} />
-      </div>
-      <button type='submit'>create</button>
-    </form>
-  )
-
-  /*
-  const BlogList = () => {
-    return (
-      <>
-        {props.blogs.map(blog =>
-          <Blog
-            key={blog.id}
-            blog={blog}
-            addLike={() => like(blog)}
-            destroyBlog={() => destroyBlog(blog)}
-            loggedInUser={user}
-          />
-        )}
-      </>
-    )
-  }
-  */
-  const blogList = () =>
-    props.blogs.map(blog =>
-      <Blog
-        key={blog.id}
-        blog={blog}
-        addLike={() => like(blog)}
-        destroyBlog={() => destroyBlog(blog)}
-        loggedInUser={props.user}
-      />
-    )
-
   return (
     <>
       <Notification />
@@ -193,14 +206,21 @@ const App = (props) => {
           {loginForm()}
         </div> :
         <div>
-          <h1>Blogs</h1>
-          <p>{props.user.name} is logged in <button onClick={() => handleLogout()}>logout</button></p>
-          <Togglable buttonLabel='new blog'>
-            <h1>Create new</h1>
-            {blogForm()}
-          </Togglable>
-          {blogList()}
-          {/* <BlogList /> */}
+          <Header user={props.user} handleLogout={() => handleLogout()} />
+          <Router>
+            <Route exact path='/' render={() =>
+              <Blogs
+                blogs={props.blogs}
+                user={props.user}
+                createBlog={props.createBlog}
+                addLike={props.addLike}
+                destroyBlog={props.destroyBlog}
+                setNotification={props.setNotification}
+                handleLogout={() => handleLogout()}
+              />
+            } />
+            <Route exact path='/users' render={() => <Users />} />
+          </Router>
         </div>
       }
     </>
