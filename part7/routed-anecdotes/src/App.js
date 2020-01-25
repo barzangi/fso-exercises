e3ea9d231ea9472aa0d896a45a68f4c24856d832
Layth Barzangi <thebarzangi@gmail.com>
2020-01-25 09:53:45 +0400

    exercises 7.1 - 7.3 routed anecdotes: added voting functionality

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { BrowserRouter as Router, Route, Link, Redirect, withRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, withRouter } from 'react-router-dom'
 
 const Menu = () => {
   const padding = {
@@ -27,10 +27,10 @@ const AnecdoteList = ({ anecdotes }) => (
   </>
 )
 
-const Anecdote = ({ anecdote }) => (
+const Anecdote = ({ anecdote, vote }) => (
   <>
     <h2>{anecdote.content}<br /><em>--{anecdote.author}</em></h2>
-    <p>has {anecdote.votes} votes</p>
+    <p>has {anecdote.votes} votes <button onClick={() => vote(anecdote)}>vote</button></p>
     <p>for more info see <a href={anecdote.info}>{anecdote.info}</a></p>
   </>
 )
@@ -40,7 +40,7 @@ const About = () => (
     <h2>About anecdote app</h2>
     <p>According to Wikipedia:</p>
 
-    <em>"An anecdote is a brief, revealing account of an individual person or an incident. Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself, such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative. An anecdote is "a story with a point."</em>
+    <em>An anecdote is a brief, revealing account of an individual person or an incident. Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself, such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative. An anecdote is "a story with a point."</em>
 
     <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
   </div>
@@ -145,15 +145,14 @@ const App = () => {
   const anecdoteById = (id) =>
     anecdotes.find(a => a.id === id)
 
-  const vote = (id) => {
-    const anecdote = anecdoteById(id)
+  const vote = (anecdote) => {
 
     const voted = {
       ...anecdote,
       votes: anecdote.votes + 1
     }
 
-    setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
+    setAnecdotes(anecdotes.map(a => a.id === anecdote.id ? voted : a))
   }
 
   return (
@@ -169,7 +168,7 @@ const App = () => {
           <Route exact path='/create' render={() => <Create addNew={addNew} updateNotification={updateNotification} />} />
           <Route exact path='/about' render={() => <About />} />
           <Route exact path='/anecdotes/:id' render={({ match }) =>
-            <Anecdote anecdote={anecdoteById(match.params.id)} />}
+            <Anecdote anecdote={anecdoteById(match.params.id)} vote={vote} />}
           />
         </div>
       </Router>
