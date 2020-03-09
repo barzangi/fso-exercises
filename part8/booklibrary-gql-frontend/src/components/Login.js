37f60f0b74921e5cbc71419010385d53da1ed6f5
Layth Barzangi <thebarzangi@gmail.com>
2020-03-10 00:21:48 +0400

    user administration: added Login component

---
+++ part8/booklibrary-gql-frontend/src/components/Login.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+
+const Login = (props) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  if (!props.show) return null
+
+  const submit = async (e) => {
+    e.preventDefault()
+    const result = await props.login({
+      variables: { username, password }
+    })
+    if (result) {
+      const token = result.data.login.value
+      console.log('token:', token)
+      props.setToken(token)
+      localStorage.setItem('phonenumbers-user-token', token)
+    }
+    setUsername('')
+    setPassword('')
+  }
+
+  return (
+    <div>
+      <h1>User Login</h1>
+      <form onSubmit={submit}>
+        <div>
+          username:
+          <input
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password:
+          <input
+            value={password}
+            type='password'
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type='submit'>log in</button>
+      </form>
+    </div>
+  )
+}
+
+export default Login
