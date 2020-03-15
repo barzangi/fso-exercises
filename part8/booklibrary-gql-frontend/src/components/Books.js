efcd514bf6557ab51c83956c0209749842978a81
Layth Barzangi <thebarzangi@gmail.com>
2020-03-15 18:13:13 +0400

    added functionality to filter books list based on genre

--- part8/booklibrary-gql-frontend/src/components/Books.js
+++ part8/booklibrary-gql-frontend/src/components/Books.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Books = (props) => {
+  const [genre, setGenre] = useState(null)
   if (props.books.loading) return <div>loading...</div>
   if (!props.show) {
     return null
@@ -8,7 +9,14 @@ const Books = (props) => {
 
   const books = props.books.data.allBooks
 
-  const padding={ padding: 5 }
+  const uniqueGenres = [ ...new Set(books.map(b => b.genres.map(g => g)).flat())]
+
+  const filteredBooks = genre ? books.filter(b => b.genres.includes(genre)) : books
+
+  const cellStyle = {
+    padding: 5,
+    textAlign: 'left'
+  }
 
   return (
     <div>
@@ -17,23 +25,33 @@ const Books = (props) => {
       <table>
         <tbody>
           <tr>
-            <th style={padding}></th>
-            <th style={padding}>
+            <th style={cellStyle}>
+              title
+            </th>
+            <th style={cellStyle}>
               author
             </th>
-            <th style={padding}>
+            <th style={cellStyle}>
               published
             </th>
           </tr>
-          {books.map(a =>
-            <tr key={a.id}>
-              <td style={padding}>{a.title}</td>
-              <td style={padding}>{a.author.name}</td>
-              <td style={padding}>{a.published}</td>
+          {filteredBooks.map(b =>
+            <tr key={b.id}>
+              <td style={cellStyle}>{b.title}</td>
+              <td style={cellStyle}>{b.author.name}</td>
+              <td style={cellStyle}>{b.published}</td>
             </tr>
           )}
         </tbody>
       </table>
+      <h3>filter by genre</h3>
+      {uniqueGenres.map(genre =>
+        <button
+          key={genre}
+          onClick={() => setGenre(genre)}
+        >{genre}</button>
+      )}
+      <button onClick={() => setGenre(null)}>all genres</button>
     </div>
   )
 }
